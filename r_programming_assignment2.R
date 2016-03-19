8504222235e6c412b3206991a840ee56d4835041
runyu <runyu.wang.2010@sis.smu.edu.sg>
2016-03-19 11:18:34 +0800

    Update r_programming_assignment2.R

--- r_programming_assignment2.R
+++ r_programming_assignment2.R
@@ -1,3 +1,5 @@
+## This function creates a special "matrix" object that can cache its inverse.
+
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
         set <- function(y) {
@@ -14,7 +16,12 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
+## This function computes the inverse of the special "matrix" created by 
+## makeCacheMatrix above. If the inverse has already been calculated (and the 
+## matrix has not changed), then it should retrieve the inverse from the cache.
+
 cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
         inv <- x$getInverse()
         if (!is.null(inv)) {
                 message("getting cached data")
